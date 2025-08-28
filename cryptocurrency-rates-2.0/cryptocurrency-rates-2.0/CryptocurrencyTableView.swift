5882da4a67cd70c47d32583ff1cf7f04738450d7
mikle <mikle.kontinent@gmail.com>
2025-08-28 13:39:06 +0700

    Start working

---
+++ cryptocurrency-rates-2.0/cryptocurrency-rates-2.0/CryptocurrencyTableView.swift
@@ -0,0 +1,12 @@
+//
+//  CryptocurrencyTableView.swift
+//  cryptocurrency-rates-2.0
+//
+//  Created by Таня Кожевникова on 28.08.2025.
+//
+
+import UIKit
+
+class CryptocurrencyTableView: UITableView {
+    
+}
